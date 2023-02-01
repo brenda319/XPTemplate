72fde4c258939bfb80dbc1c39f803fb137163f42
Brenda Rodriguez <brendarodriguez@Brendas-MacBook-Pro.local>
2023-02-01 12:30:45 -0600

    feat: configuration files and jumpstarter added to bin folder

---
+++ test/dummy.test.js
@@ -0,0 +1,7 @@
+const template = require('../src/dummy');
+
+describe('Test dummy description', () => {
+  it('should chaange for a test description', () => {
+    expect(template()).toBe();
+  });
+});
